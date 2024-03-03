30d617071e4bc2a045dbc5481d2bd383a5fc8c4a
yura.filionchik@gmail.com <yura.filionchik@gmail.com>
2024-03-03 10:33:11 +0100

    add autofocus

---
+++ TodoList/wwwroot/scripts/AutoFocus.js
@@ -0,0 +1,7 @@
+﻿let SetCustomFocus = SetCustomFocus || {};
+SetCustomFocus.setFocus = function (elementId) {
+    var element = document.getElementById(elementId);
+    if (element) {
+        element.focus();
+    }
+};
