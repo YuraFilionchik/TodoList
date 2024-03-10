2c96f5ab2aaf8e560c84e995bb0f5be0bef93180
yura.filionchik <yura.filionchik@gmail.com>
2024-03-10 12:53:22 +0100

    add ViewEditItem +bootstrap

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
+builder.Services.AddBlazorBootstrap();
 
 builder.Services.AddAuthentication(options =>
     {
